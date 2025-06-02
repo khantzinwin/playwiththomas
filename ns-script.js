97c3e4b76fdd2b292bf611bf6a9ba97c07509703
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-02 14:04:48 +0700

    update GH

--- ns-script.js
+++ ns-script.js
@@ -335,10 +335,11 @@ const products = [
         image: "images/ps4andps5/gta-trilogy.png" 
       },
 
+      //sale June 11, 2025
       { 
-        name: "Green Hell", 
+        name: "Green Hell [Primary]", 
         version: "<br>Version - NS",
-        price: "<br> ❌ PA - 10,000ks <br>✅ NA - 5,000ks", 
+        price: "<br> ✅ PA - 10,000ks <br>✅ NA - 5,000ks", 
         image: "images/nintendo-switch/green-hell.png" 
       },
 
