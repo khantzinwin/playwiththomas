aa80a7fe85941051e7bf5dad0396db51325dfd70
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-05 22:45:12 +0700

    update nightreign

--- script.js
+++ script.js
@@ -283,7 +283,7 @@ const products = [
       { 
         name: "Elden Ring Nightreign", 
         version: "<br>Version - PS5 & PS4",
-        price: "<br> ❌ PS5AA - 60,000ks <br>✅ PS4AA - 60,000ks <br> ✅ DA - 40,000ks", 
+        price: "<br> ❌ PS5AA - 60,000ks <br>✅ PS4AA - 60,000ks <br> ❌ DA - 40,000ks", 
         image: "images/ps4andps5/elden-ring-nightreign.png" 
       },
 
