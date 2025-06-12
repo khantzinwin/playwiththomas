0ab5aadd9581efde286a7971ec951de41779a210
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-12 18:30:09 +0700

    update tlous2 re

--- script.js
+++ script.js
@@ -1189,7 +1189,7 @@ const products2 = [
       { 
         name: "The Last of Us Part II Remastered", 
         version: "<br>Version - PS5",
-        price: "<br> ❌ PS5AA - 100,000ks <br> ❌ PS5DA - 50,000ks", 
+        price: "<br> ❌ PS5AA - 100,000ks <br> ✅ PS5DA - 50,000ks", 
         image: "images/ps4andps5/tlous2-remastered.png" 
       },
 
