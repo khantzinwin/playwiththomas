8653850ecf1d0382a2ce3b74d0e6358dddbabbec
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-12 13:22:59 +0700

    update re village

--- script.js
+++ script.js
@@ -738,7 +738,7 @@ const products = [
       { 
         name: "Resident Evil Village Gold Edition", 
         version: "<br>Version - PS5 & PS4",
-        price: "<br> ❌ PS5AA - 35,000ks <br>❌ PS4AA - 35,000ks <br> ✅ DA - 20,000ks", 
+        price: "<br> ❌ PS5AA - 35,000ks <br>❌ PS4AA - 35,000ks <br> ❌ DA - 20,000ks", 
         image: "images/ps4andps5/revillage-ge.png" 
       },
 
