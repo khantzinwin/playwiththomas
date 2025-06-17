d7d6cc2f349827bbd801e5fd73a017521079c331
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-17 14:13:07 +0700

    update until dawn

--- script.js
+++ script.js
@@ -941,7 +941,7 @@ const products = [
       { 
         name: "Until Dawn", 
         version: "<br>Version - PS4",
-        price: "<br> ✅ PS5AA - 20,000ks <br>✅ PS4AA - 20,000ks <br> ✅ DA - 10,000ks", 
+        price: "<br> ❌ PS5AA - 20,000ks <br>❌ PS4AA - 20,000ks <br> ❌ DA - 10,000ks", 
         image: "https://storage.googleapis.com/pwt-img/ps4andps5/until-dawn.png" 
       },
 
