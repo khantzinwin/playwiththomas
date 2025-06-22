fcbd98e8c0e8f01bad0965e5b51c7e60cf4fe3f3
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-22 13:03:04 +0700

    update the last guardians

--- script.js
+++ script.js
@@ -864,7 +864,7 @@ const products = [
       { 
         name: "The Last Guardian", 
         version: "<br>Version - PS4",
-        price: "<br> ❌ PS5AA - 20,000ks <br>✅ PS4AA - 20,000ks <br> ✅ DA - 10,000ks", 
+        price: "<br> ❌ PS5AA - 20,000ks <br>✅ PS4AA - 20,000ks <br> ❌ DA - 10,000ks", 
         image: "https://storage.googleapis.com/pwt-img/ps4andps5/the-last-guardian.png" 
       },
 
