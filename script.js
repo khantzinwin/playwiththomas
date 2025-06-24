7edbbd4fc248709d82f0fe563fb86c5c3ffa45e3
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-24 19:47:34 +0700

    ffxvi

--- script.js
+++ script.js
@@ -1091,7 +1091,7 @@ const products2 = [
       { 
         name: "Final Fantasy XVI", 
         version: "<br>Version - PS5",
-        price: "<br> ❌ PS5AA - 100,000ks <br> ✅ PS5DA - 50,000ks", 
+        price: "<br> ❌ PS5AA - 100,000ks <br> ❌ PS5DA - 50,000ks", 
         image: "https://storage.googleapis.com/pwt-img/ps4andps5/ffxvi.png" 
       },
 
