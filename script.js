d07091e397fb7e2e5725ba03c4c3a5cea82ab283
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-26 15:56:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1215,10 +1215,10 @@ const products2 = [
       },
 
       { 
-        name: "Wuchang Fallen Feathers Digital Deluxe Edition [Pre Order]", 
+        name: "Wuchang Fallen Feathers Deluxe Edition [Pre Order]", 
         version: "<br>Version - PS5",
         price: "<br> ✅ PS5AA - 140,000ks <br> ✅ PS5DA - 70,000ks", 
-        image: "https://storage.googleapis.com/pwt-img/ps4andps5/Wuchang%20Fallen%20Feathers.png" 
+        image: "https://storage.googleapis.com/pwt-img/ps4andps5/Wuchang%20Fallen%20Feathers%20(1).png" 
       }
 ];
 
