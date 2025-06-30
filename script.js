d5cbf7d2b7d1c91c66ab2d5e2dc67f74514c338c
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-06-30 14:28:11 +0700

    gow3

--- script.js
+++ script.js
@@ -360,7 +360,7 @@ const products = [
       { 
         name: "God of War III Remastered", 
         version: "<br>Version - PS4",
-        price: "<br> ✅ PS5AA - 20,000ks <br>❌ PS4AA - 20,000ks <br> ❌ DA - 10,000ks", 
+        price: "<br> ❌ PS5AA - 20,000ks <br>❌ PS4AA - 20,000ks <br> ❌ DA - 10,000ks", 
         image: "https://storage.googleapis.com/pwt-img/ps4andps5/gow-iii.png" 
       },
 
