0888bda348d9f57847792679c65acb2da35cd866
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-07-06 17:51:43 +0700

    update lego dc

--- script.js
+++ script.js
@@ -479,7 +479,7 @@ const products = [
       { 
         name: "LEGO DC Super Villains", 
         version: "<br>Version - PS4",
-        price: "<br> ❌ PS5AA - 20,000ks <br>❌ PS4AA - 20,000ks <br> ✅ DA - 10,000ks", 
+        price: "<br> ❌ PS5AA - 20,000ks <br>❌ PS4AA - 20,000ks <br> ❌ DA - 10,000ks", 
         image: "https://storage.googleapis.com/pwt-img/ps4andps5/lego-super-villains.png" 
       },
 
