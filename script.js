0f372a42ac7854594def53ac2ee8a6080152dce1
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-07-09 21:27:00 +0700

    update detroit

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ const products = [
       { 
         name: "Detroit Become Human", 
         version: "<br>Version - PS4",
-        price: "<br> ❌ PS5AA - 20,000ks <br>❌ PS4AA - 20,000ks <br> ✅ DA - 10,000ks", 
+        price: "<br> ❌ PS5AA - 20,000ks <br>❌ PS4AA - 20,000ks <br> ❌ DA - 10,000ks", 
         image: "https://storage.googleapis.com/pwt-img/ps4andps5/detroit-become-human.png" 
       },
 
