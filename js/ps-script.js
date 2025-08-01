5dc1a0cf67543f68e56aa11e40e696315d92e7ec
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-08-01 13:58:35 +0700

    update-fc25-ps

--- js/ps-script.js
+++ js/ps-script.js
@@ -325,7 +325,7 @@ const products = [
       { 
         name: "FC 25", 
         version: "<br>Version - PS5 & PS4",
-        price: "<br> ✅ PS5AA - 30,000ks <br>❌ PS4AA - 30,000ks <br> ❌ DA - 15,000ks", 
+        price: "<br> ❌ PS5AA - 30,000ks <br>❌ PS4AA - 30,000ks <br> ❌ DA - 15,000ks", 
         image: "https://storage.googleapis.com/pwt-img/ps4andps5/fc-25.png" 
       },
 
