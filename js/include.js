50b1f5dd9ca844343c0d54e06bf5dbe4d57ec7c8
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-08-04 00:12:31 +0700

    update-refractore

--- js/include.js
+++ js/include.js
@@ -1,12 +1,2 @@
-function loadHTML(id, url) {
-  fetch(url)
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById(id).innerHTML = data;
-    });
-}
-
-window.onload = function() {
-  loadHTML('header', 'header.html');
-  loadHTML('footer', 'footer.html');
-};
+function loadHTML(id,url){fetch(url).then(res=>res.text()).then(data=>{document.getElementById(id).innerHTML=data})}
+window.onload=function(){loadHTML('header','header.html');loadHTML('footer','footer.html')}
