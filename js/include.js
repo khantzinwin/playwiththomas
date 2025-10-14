40434d1b2e8b4b8a8cd50534451c463df166a0d4
Khantzin Win <137606155+khantzinwin@users.noreply.github.com>
2025-10-14 17:32:01 +0700

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,2 +1,6 @@
-function loadHTML(id,url){fetch(url).then(res=>res.text()).then(data=>{document.getElementById(id).innerHTML=data})}
-window.onload=function(){loadHTML('header','header.html');loadHTML('footer','footer.html')}
+// Load header.html dynamically
+fetch("header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
